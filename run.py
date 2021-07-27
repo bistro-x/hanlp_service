1e3d3b8d32a18ef46bd2066f07695b1bfe12f18c
chenyongqin <chenyongqin@dataknown.cn>
2021-07-27 08:56:56 +0800

    提交分词解析接口

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import hanlp
 import os
 from flask import request, jsonify, Flask
 
-
 hanlp_engine = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
 
 app = Flask(__name__, root_path=os.getcwd())
@@ -18,6 +17,15 @@ def participle():
     return jsonify({"result": result})
 
 
-if __name__ == '__main__':
+@app.route('/lexical', methods=["POST"])
+def lexical():
+    param = {**(request.form or {}), **(request.json or {})}
+    sentence = param.get('sentence', '')
 
+    result = hanlp_engine(sentence, tasks='pos/pku').get('pos/pku')
+
+    return jsonify({"result": result})
+
+
+if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
